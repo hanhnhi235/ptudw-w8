cffd32aa0b1f4f74d2f047e72a6b18bf9a0bf2a9
Duong Hanh Nhi <dhnhi21@clc.fitus.edu.vn>
2023-11-26 04:35:00 +0700

    huhu lam duoc paginate r

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,62 +2,152 @@ const controller = {};
 const models = require("../models");
 const Sequelize = require("sequelize"); 
 const Op = Sequelize.Op;
+const limit = 2;
+const loadBlogs = datas => datas.map(data => data.dataValues);
 
 controller.showList = async (req, res) => {
-	let category = req.query.category;
-	let tag = req.query.tag;
-	let search = req.query.search;
-	if (category) {
-		res.locals.blogs = await models.Blog.findAll({
-			attributes: ["id", "title", "imagePath", "summary", "createdAt"],
-			where: { categoryId: category },
-			include: [
-				{ model: models.Comment },		
-			],
-		})
-	} 
-	else if (tag) {
-		let tagBlog = await models.Tag.findByPk(tag);
-		res.locals.blogs = await tagBlog.getBlogs({
-			attributes: ["id", "title", "imagePath", "summary", "createdAt"],
-			include: [
-				{ model: models.Comment },
-			]
-		})
-	}
 
-	else if (search) {
-		res.locals.blogs = await models.Blog.findAll({
-			attributes: ["id", "title", "imagePath", "summary", "createdAt"],
-			where: { title: { [Op.iRegexp]: search }},
-			include: [
-				{ model: models.Comment },		
-			],
-		})
-	}
-	else {
-		res.locals.blogs = await models.Blog.findAll({
-			attributes: ["id", "title", "imagePath", "summary", "createdAt"],
-			include: [
-				{ model: models.Comment },		
-			],
-		})
-	}
-	
 	res.locals.categories = await models.Category.findAll({
 		attributes: ["id", "name"],
 		include: [
 			{ model: models.Blog},
 		]
-	}),
+	});
 	res.locals.tags = await models.Tag.findAll({
 		attributes: ["id", "name"],
-	}),
+	});
 
-	res.render("index");
+	let category = req.query.category;
+	let tag = req.query.tag;
+	let search = req.query.search;
+	let page = Number(req.query.page) || 1;
+
+	if (category) {
+		let { count, rows: blogs } = await models.Blog.findAndCountAll({
+			limit: limit,
+			offset: (page - 1) * limit,
+			attributes: 
+			[	"id", "title", "imagePath", "summary", "createdAt",
+				[
+					models.Sequelize.literal(
+						'(SELECT COUNT(*) FROM "Comments" WHERE "Comments"."blogId" = "Blog"."id")'
+						),
+						"comments",
+				],
+			],
+			where: { categoryId: category },
+		});
+		res.render(
+			"index", 
+			{ 
+				blogs: loadBlogs(blogs),
+				pagination: {
+					page: page,
+					limit: limit,
+					totalRows: count,
+					queryParams: { category: category }, 
+				}
+			});
+	} 
+	else if (tag) {
+		let { count, rows: blogs } = await models.Blog.findAndCountAll({
+			limit: limit,
+			offset: (page - 1) * limit,
+			attributes: 
+			[	"id", "title", "imagePath", "summary", "createdAt",
+				[
+					models.Sequelize.literal(
+						'(SELECT COUNT(*) FROM "Comments" WHERE "Comments"."blogId" = "Blog"."id")'
+						),
+						"comments",
+				],
+			],
+			include: [
+				{
+					model: models.Tag,
+					where: { id: tag },
+				}
+			]
+		});
+		res.render(
+			"index", 
+			{ 
+				blogs: loadBlogs(blogs),
+				pagination: {
+					page: page,
+					limit: limit,
+					totalRows: count, 
+					queryParams: { tag: tag },
+				}
+			});
+	}
+
+	else if (search) {
+		let { count, rows: blogs } = await models.Blog.findAndCountAll({
+			limit: limit,
+			offset: (page - 1) * limit,
+			attributes: 
+			[	"id", "title", "imagePath", "summary", "createdAt",
+				[
+					models.Sequelize.literal(
+						'(SELECT COUNT(*) FROM "Comments" WHERE "Comments"."blogId" = "Blog"."id")'
+						),
+						"comments",
+				],
+			],
+			where: { title: { [Op.iRegexp]: search }},
+		});
+		res.render(
+			"index", 
+			{ 
+				blogs: loadBlogs(blogs),
+				pagination: {
+					page: page,
+					limit: limit,
+					totalRows: count,
+					queryParams: { search: search }, 
+				}
+			});
+
+	}
+	else {
+		let { count, rows: blogs} = await models.Blog.findAndCountAll({
+			limit: limit,
+			offset: (page - 1) * limit,
+			attributes: 
+			[	"id", "title", "imagePath", "summary", "createdAt",
+				[
+					models.Sequelize.literal(
+						'(SELECT COUNT(*) FROM "Comments" WHERE "Comments"."blogId" = "Blog"."id")'
+						),
+						"comments",
+				],
+			],
+		});
+		res.render(
+			"index", 
+			{ 
+				blogs: loadBlogs(blogs),
+				pagination: {
+					page: page,
+					limit: limit,
+					totalRows: count, 
+				}
+			});
+	}
 };
 
 controller.showDetails = async (req, res) => {
+	res.locals.categories = await models.Category.findAll({
+		attributes: ["id", "name"],
+		include: [
+			{ model: models.Blog},
+		]
+	});
+	res.locals.tags = await models.Tag.findAll({
+		attributes: ["id", "name"],
+	});
+	
 	let id = isNaN(req.params.id) ? 0 : parseInt(req.params.id);
 	res.locals.blog = await models.Blog.findOne({
 		attributes: ["id", "title", "description", "createdAt", "imagePath"],
@@ -69,6 +159,17 @@ controller.showDetails = async (req, res) => {
 			{ model: models.Comment },
 		],
 	})
+
+	res.locals.categories = await models.Category.findAll({
+		attributes: ["id", "name"],
+		include: [
+			{ model: models.Blog},
+		]
+	});
+	res.locals.tags = await models.Tag.findAll({
+		attributes: ["id", "name"],
+	});
+
 	res.render("details");
 };
 
--- index.js
+++ index.js
@@ -2,31 +2,35 @@ const express = require("express");
 const app = express();
 const port = 4000 | process.env.PORT;
 const expressHbs = require('express-handlebars');
-
+const paginate = require('express-handlebars-paginate');
 app.use(express.static(__dirname + "/html"));
 
-app.engine(
-	"hbs", 
-	expressHbs.engine ({
-	layoutsDir: __dirname + "/views/layouts",
-	partialsDir: __dirname + "/views/partials",
-	defaultLayout: "layout",
-	extname: "hbs",
-	runtimeOptions: {
-		allowProtoPropertiesByDefault: true,
-	},
-	helpers: {
-		showDate: (date) => { 
-			return date.toLocaleDateString('en-US', {
-				year: 'numeric',
-				month: 'long',
-				day: 'numeric',
-			});
-		},
-	},
-})
+app.engine(   
+	"hbs",    
+	expressHbs.engine ({     
+		layoutsDir: __dirname + "/views/layouts",     
+		partialsDir: __dirname + "/views/partials",     
+		defaultLayout: "layout",     
+		extname: "hbs",     
+		runtimeOptions: {       
+			allowProtoPropertiesByDefault: true,     
+		},     
+		helpers: {       
+			showDate: (date) => {          
+				return date.toLocaleDateString(
+					'en-US', {           
+						year: 'numeric',           
+						month: 'long',           
+						day: 'numeric',         
+					});       
+				},    
+			paginateHelper: paginate.createPagination,   
+			},   
+		}),
 );
+
 app.set("view engine", "hbs");
+	
 
 app.get("/", (req, res) => res.redirect("/blogs"));
 app.use("/blogs", require("./routes/blogRouter"))
